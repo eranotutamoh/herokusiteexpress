8fdb500b0bcb6ed1eba2b5c7f950db93ad35a057
eranotutamoh <jellynull@gmail.com>
2016-03-21 12:03:37 +1300

    Database added

--- app_server/controllers/recipe.js
+++ app_server/controllers/recipe.js
@@ -1,5 +1,12 @@
 module.exports.search = function (req, res) {
-    res.render('search', { title: 'Recipe Search' });
+    res.render('search', {
+        title: 'Recipe Search',
+        text: {
+            h1: 'Search',
+            info: 'Enter an ingredient'
+        },
+        ingredients: ['broccoli','carrot','sour cream']
+    });
 };
 module.exports.recipe = function (req, res) {
     res.render('recipe', { title: 'A Recipe' });
---
+++ app_server/models/recipes.js
@@ -0,0 +1,23 @@
+var mongoose = require( 'mongoose' );
+
+var ingredientSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+});
+
+var ingredientsSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    quantity: String
+});
+
+var recipeSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    ingredients: [ingredientsSchema],
+    instructions: String
+});
+
+
+mongoose.model('Ingredient', ingredientSchema);
+mongoose.model('Recipes', recipeSchema);
+
+
+
