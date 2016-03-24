d2c84bc4e732ce3aaa1c936c0def50e9a42dfaad
eranotutamoh <jellynull@gmail.com>
2016-03-24 19:26:46 +1300

    Testing ajax call

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,13 +1,30 @@
 var input = document.querySelector('#search');
 
 input.addEventListener('keyup' , function() {
-
+    console.log('Rockin')
 
     var ajax = new XMLHttpRequest();
 
     ajax.open('GET', '/api/ingredients?ing='+this.value, true);
 
     ajax.addEventListener('load', downloadFinished, false);
+    ajax.addEventListener('loadstart', onLoadStart, false);
+    ajax.addEventListener('progress', onProgress, false);
+    ajax.addEventListener('error', onError, false);
+    ajax.addEventListener('abort', onAbort, false);
+
+    function onLoadStart(evt) {
+        console.log('onLoadStart')
+    }
+    function onProgress(evt) {
+        console.log('onProgress')
+    }
+    function onError(evt) {
+        console.log('onError')
+    }
+    function onAbort(evt) {
+        console.log('onAbort')
+    }
 
     function downloadFinished(evt){
         if(this.status == 200){
