247854c6690230570263bd0356dad8d7188e1c94
eranotutamoh <jellynull@gmail.com>
2016-04-12 12:37:26 +1200

    End of Express chapter  Start of git + code section

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,3 +1,7 @@
 module.exports.index = function (req, res) {
-    res.render('index', { title: 'Expresso Bongos!' });
+    res.render('index', { title: 'Jonathan Pierce' });
+};
+
+module.exports.git = function (req, res) {
+    res.render('code/git', { title: 'GIT Commands' });
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,9 +4,12 @@ var router = express.Router();
 var home = require('../controllers/main');
 var recipes = require('../controllers/recipe');
 
-console.log('Using the HTML routes');
-
+// Home site
 router.get('/', home.index);
+router.get('/git', home.git);
+
+
+// Sub site Recipes
 router.get('/recipes', recipes.listrecipes);
 router.get('/recipesearch', recipes.search);
 router.get('/recipe/:recipeid', recipes.recipe);
@@ -15,5 +18,7 @@ router.post('/recipeedit/:recipeid/:edit', recipes.editpost);
 router.get('/recipeadd', recipes.add);
 router.post('/recipeadd', recipes.addpost);
 
+
+router.get('/', home.index);
 
 module.exports = router;
